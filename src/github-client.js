5eab142ea0486902da398b2ca0f040407f434ccf
Bruno Pinto <brosapinto@gmail.com>
2019-08-08 11:19:06 +0100

    Converts GitHubClientProvider to hooks

--- src/github-client.js
+++ src/github-client.js
@@ -1,13 +1,13 @@
 /* @jsx jsx */
 import {jsx} from '@emotion/core'
 
-import React from 'react'
+import {createContext, useState, useEffect} from 'react'
 import {navigate, createHistory} from '@reach/router'
 import netlify from 'netlify-auth-providers'
 import {GraphQLClient} from 'graphql-request'
 import {PrimaryButton} from './shared/pattern'
 
-const GitHubClientContext = React.createContext()
+const GitHubClientContext = createContext()
 const {Provider, Consumer} = GitHubClientContext
 
 async function authWithGitHub() {
@@ -29,84 +29,84 @@ async function authWithGitHub() {
 
 const history = createHistory(window)
 
-class GitHubClientProvider extends React.Component {
-  constructor(...args) {
-    super(...args)
-    this.state = {error: null}
-    if (this.props.client) {
-      this.state.client = this.props.client
-    } else {
-      const token = window.localStorage.getItem('github-token')
-      if (token) {
-        this.state.client = this.getClient(token)
-      }
+function GitHubClientProvider(props) {
+  const [error, setError] = useState(null)
+  // useState has an initializer function which is
+  // called only once when the component mounts
+  const [client, setClient] = useState(() => {
+    if (props.client) {
+      return props.client
     }
-  }
-  componentDidMount() {
-    if (!this.state.client) {
-      navigate('/')
+
+    const token = window.localStorage.getItem('github-token')
+    if (token) {
+      return getClient(token)
     }
-    this.unsubscribeHistory = history.listen(() => {
-      if (!this.state.client) {
-        navigate('/')
-      }
-    })
-  }
-  componentWillUnmount() {
-    this.unsubscribeHistory()
-  }
-  getClient = token => {
+  })
+
+  function getClient(token) {
     const headers = {Authorization: `bearer ${token}`}
     const client = new GraphQLClient('https://api.github.com/graphql', {
       headers,
     })
     return Object.assign(client, {
-      login: this.login,
-      logout: this.logout,
+      login,
+      logout,
     })
   }
-  logout = () => {
+
+  useEffect(() => {
+    if (!client) {
+      navigate('/')
+    }
+
+    const unsubscribeHistory = history.listen(() => {
+      if (!client) {
+        navigate('/')
+      }
+    })
+
+    return () => unsubscribeHistory()
+  }, [])
+
+  function logout() {
     window.localStorage.removeItem('github-token')
-    this.setState({client: null, error: null})
+    setError(null)
+    setClient(null)
     navigate('/')
   }
-  login = async () => {
+
+  async function login() {
     const data = await authWithGitHub().catch(error => {
-      console.log('Oh no', error)
-      this.setState({error})
+      console.error('Oh no', error)
+      setError(error)
     })
     window.localStorage.setItem('github-token', data.token)
-    this.setState({client: this.getClient(data.token)})
+    setClient(getClient(data.token))
   }
-  render() {
-    const {client, error} = this.state
-    const {children} = this.props
 
-    return client ? (
-      <Provider value={client}>{children}</Provider>
-    ) : (
-      <div
-        css={{
-          marginTop: 250,
-          display: 'flex',
-          justifyContent: 'center',
-        }}
-      >
-        {error ? (
-          <div>
-            <p>Oh no! There was an error.</p>
-            <pre>{JSON.stringify(error, null, 2)}</pre>
-          </div>
-        ) : (
-          <div>
-            <PrimaryButton onClick={this.login}>
-              Login with GitHub
-            </PrimaryButton>
-          </div>
-        )}
-      </div>
-    )
-  }
+  return client ? (
+    <Provider value={client}>{props.children}</Provider>
+  ) : (
+    <div
+      css={{
+        marginTop: 250,
+        display: 'flex',
+        justifyContent: 'center',
+      }}
+    >
+      {error ? (
+        <div>
+          <p>Oh no! There was an error.</p>
+          <pre>{JSON.stringify(error, null, 2)}</pre>
+        </div>
+      ) : (
+        <div>
+          <PrimaryButton onClick={login}>Login with GitHub</PrimaryButton>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export {
